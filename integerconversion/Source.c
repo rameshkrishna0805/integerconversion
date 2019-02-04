6a521ad23de6c9e87ac5770f291096b991643c7c
rameshkrishna0805 <vijayarameshkrishna@gmail.com>
2019-02-03 22:27:01 -0800

    integerconversion

---
+++ integerconversion/Source.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int n;
+	int count = 0, min = 99999, max = 0, sum = 0;
+	while (1)
+	{
+		
+		scanf_s(" %d", &n);
+		if (n > 0)
+		{
+			count++;
+			sum = sum + n;
+			if (n > max)
+			{
+				max = n;
+			}
+			else if(n<min)
+			{
+				min = n;
+			}
+		}
+		if (n <= 0)
+		{
+			break;
+		}
+
+	}
+	printf("\n %d %d %d %d\n", count, min, max,sum/count);
+	//getch();
+		return 0;
+
+}
